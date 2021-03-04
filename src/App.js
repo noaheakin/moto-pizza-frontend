33a3928d7bfda40c5660e3f7f4aa0ab1b5ffde25
noaheakin <noaheakin@gmail.com>
2021-03-03 22:58:59 -0800

    began building out NavBar and Home components, implement routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { Route, Switch, withRouter } from 'react-router-dom';
+// import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app">
+        {/* <NavBar /> */}
+        <Switch>
+          <Route exact path='/' component={Home} />        
+        </Switch>
         <h1>Hello, World!</h1>
       </div>
     )
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Hello, Pizza Lovers!
+            </div>
+        )
+    }
+}
+
+export default Home;
